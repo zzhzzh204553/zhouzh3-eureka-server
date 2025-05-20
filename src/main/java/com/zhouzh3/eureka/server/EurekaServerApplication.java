87350276f0e4c74f1548e47e1417bba95c950100
zhouzhenhua <zhouzhenhua1984@163.com>
2025-05-20 13:18:44 +0800

    初次提交

---
+++ src/main/java/com/zhouzh3/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhouzh3.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(scanBasePackages = "com.zhouzh3.eureka.**")
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
